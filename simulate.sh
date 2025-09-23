7e3f85374804c303090e5a08a28739fd98d1bb16
haydenqw <haydenflores25@gmail.com>
2025-09-23 17:40:23 -0300

    Agrego script simulate.sh para simulación de neutrinos

---
+++ simulate.sh
@@ -0,0 +1,23 @@
+ #!/bin/bash
+
+start=$(date +%s)
+
+for i in $(seq 1 1000)
+do
+  filename=$(printf "%04d.txt" $i)
+  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  neutrinos=$((RANDOM % 11))
+  echo "$timestamp - neutrinos: $neutrinos" > "$filename"
+done
+
+cat *.txt > results.txt
+
+end=$(date +%s)
+runtime=$((end - start))
+
+echo "Tiempo total: $runtime segundos" > performance.txt
+echo "Tiempo promedio por evento: $(echo "scale=3; $runtime/1000" | bc) segundos" >> performance.txt
+
+
+
+
